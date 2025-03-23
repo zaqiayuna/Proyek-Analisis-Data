60a9b34382aa5c4d70b1318d6c414af6ee5730db
zaqiayuna <102151016+zaqiayuna@users.noreply.github.com>
2025-03-23 11:13:00 +0700

    Update dasboard.py

--- Dasboard/dasboard.py
+++ Dasboard/dasboard.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
-# Set style seaborn
 sns.set(style='dark')
 
 # Menyiapkan data day_df
@@ -120,7 +119,7 @@ with st.sidebar:
 
     # Mengambil start_date & end_date dari date_input
     start_date, end_date = st.date_input(
-        label='Rentang Waktu',
+        label='Timeline',
         min_value= min_date,
         max_value= max_date,
         value=[min_date, max_date]
@@ -141,56 +140,9 @@ holiday_rent_df = create_holiday_rent_df(main_df)
 weather_rent_df = create_weather_rent_df(main_df)
 
 
-# Membuat Dashboard secara lengkap
 
-# Membuat judul
-st.header('Bike Rental Dashboard 🚲')
-
-# Membuat jumlah penyewaan harian
-st.subheader('Daily Rentals')
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    daily_rent_casual = daily_casual_rent_df['casual'].sum()
-    st.metric('Casual User', value= daily_rent_casual)
-
-with col2:
-    daily_rent_registered = daily_registered_rent_df['registered'].sum()
-    st.metric('Registered User', value= daily_rent_registered)
- 
-with col3:
-    daily_rent_total = daily_rent_df['count'].sum()
-    st.metric('Total User', value= daily_rent_total)
-
-# Membuat jumlah penyewaan bulanan
-st.subheader('Monthly Rentals')
-fig, ax = plt.subplots(figsize=(24, 8))
-
-# Plot total count
-ax.plot(
-    monthly_rent_df.index,
-    monthly_rent_df['count'],
-    marker='o', 
-    linewidth=2,
-    color='tab:blue',
-    label='Total Count'
-)
-
-# Plot average count
-average_count = monthly_rent_df['count'].mean()
-ax.axhline(y=average_count, color='tab:red', linestyle='--', label='Average Count')
-
-# Annotate total count
-for index, row in enumerate(monthly_rent_df['count']):
-    ax.text(index, row + 1, str(row), ha='center', va='bottom', fontsize=12)
-
-# Annotate average count
-ax.text(len(monthly_rent_df) - 1, average_count + 1, f'Avg: {average_count:.2f}', ha='center', va='bottom', fontsize=12, color='tab:red')
-
-ax.tick_params(axis='x', labelsize=25, rotation=45)
-ax.tick_params(axis='y', labelsize=20)
-ax.legend()
-st.pyplot(fig)
+# judul
+st.header('Dasboard Rental Sepeda')
 
 
 # Membuah jumlah penyewaan berdasarkan kondisi cuaca
@@ -198,7 +150,9 @@ st.subheader('Weatherly Rentals')
 
 fig, ax = plt.subplots(figsize=(16, 8))
 
-colors=["tab:blue", "tab:orange", "tab:green"]
+
+#10 warna 
+colors=["tab:red", "tab:green", "tab:pink"]
 
 sns.barplot(
     x=weather_rent_df.index,
@@ -221,9 +175,11 @@ st.subheader('Weekday, Workingday, and Holiday Rentals')
 
 fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(15,10))
 
-colors1=["tab:blue", "tab:orange"]
-colors2=["tab:blue", "tab:orange"]
-colors3=["tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple", "tab:brown", "tab:pink"]
+
+#ganti warna 
+colors1=["tab:red", "tab:green"]
+colors2=["tab:red", "tab:green"]
+colors3=["tab:pink", "tab:red", "tab:pink", "tab:red", "tab:pink", "tab:red", "tab:pink"]
 
 # Berdasarkan workingday
 sns.barplot(
